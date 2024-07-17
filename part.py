3136488b855148ace86abd20e7c61773351a9a49
Muhammad Achad Ansori <94736530+achadansori@users.noreply.github.com>
2024-07-17 10:33:57 +0700

    Update part.py

--- part.py
+++ part.py
@@ -1,9 +1,6 @@
 import cv2
 import numpy as np
 import time
-import serial
-
-ser = serial.Serial('/dev/ttyUSB0', 9600)
 
 def detect_objects():
     net = cv2.dnn.readNet("yolov4-tiny-custom_last.weights", "yolov4-tiny-custom.cfg")
@@ -12,13 +9,10 @@ def detect_objects():
     with open("obj.names", "r") as f:
         classes = [line.strip() for line in f.readlines()]
 
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(0)
 
     start_time = time.time()
     frame_count = 0
-
-    part_depan_value = 10
-    part_belakang_value = 20
 
     while True:
         ret, frame = cap.read()
@@ -55,26 +49,6 @@ def detect_objects():
                     confidences.append(float(confidence))
                     class_ids.append(class_id)
 
-                    # Mengirim nilai part_depan_value dan part_belakang_value ke Arduino
-                    if classes[class_id] == "part depan":
-                        if center_x > width // 2:
-                            if center_x > width - 50:
-                                part_depan_value = 10
-                            else:
-                                part_depan_value = 11
-                        else:
-                            part_depan_value = 10
-                        ser.write(str(part_depan_value).encode())  # Mengirim nilai part_depan_value ke Arduino
-                    elif classes[class_id] == "part belakang":
-                        if center_x > width // 2:
-                            if center_x > width - 50:
-                                part_belakang_value = 20
-                            else:
-                                part_belakang_value = 21
-                        else:
-                            part_belakang_value = 20
-                        ser.write(str(part_belakang_value).encode())  # Mengirim nilai part_belakang_value ke Arduino
-
         indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.3, 0.4)
 
         if len(indices) > 0:
@@ -90,19 +64,6 @@ def detect_objects():
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 cv2.putText(frame, label, (x + 5, y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
-                # Calculate center coordinates of the bounding box
-                center_x_box = x + w // 2
-                center_y_box = y + h // 2
-
-                # Draw center point
-                cv2.circle(frame, (center_x_box, center_y_box), 5, (255, 0, 0), -1)
-
-        # Draw vertical line in the middle of the frame
-        cv2.line(frame, (width // 2, 0), (width // 2, height), (0, 255, 255), 2)
-        
-        # Draw vertical line on the right side of the frame
-        cv2.line(frame, (width - 50, 0), (width - 50, height), (255, 0, 255), 2)
-
         # Calculate and display FPS
         end_time = time.time()
         elapsed_time = end_time - start_time
@@ -111,9 +72,6 @@ def detect_objects():
         # Display FPS on the screen
         cv2.putText(frame, f"FPS: {fps:.2f}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
-        # Print the value of "part depan"
-        cv2.putText(frame, f"Part Depan Value: {part_depan_value}", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(frame, f"Part Belakang Value: {part_belakang_value}", (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
         cv2.imshow("YOLO Object Detection", frame)
         if cv2.waitKey(1) == ord('q'):
             break
